2fb91e8c0139d69a07da49bc2ddd5ac1a7f93452
Gozde SAYGILI YALCIN <saygiligozde@gmail.com>
2022-09-04 23:20:48 +0300

    solution is added

---
+++ src/com/github/hackerrank/thirtdayschallenge/SolutionDayOne.java
@@ -0,0 +1,16 @@
+package com.github.hackerrank.thirtdayschallenge;
+
+import java.util.Scanner;
+
+public class SolutionDayOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World. ");
+		Scanner inputString = new Scanner(System.in);
+		System.out.println(inputString.nextLine());
+		inputString.close();
+
+	}
+
+}
